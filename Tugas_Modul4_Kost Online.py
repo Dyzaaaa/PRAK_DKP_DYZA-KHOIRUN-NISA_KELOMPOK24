afdd09555497b7efbf5277efeb6f6a0f7ff3e63b
sitiisyaroh <102467384+sitiisyaroh@users.noreply.github.com>
2022-03-27 22:57:02 +0700

    Update Tugas_Modul4_Kost Online.py

--- Tugas_Modul4_Kost Online.py
+++ Tugas_Modul4_Kost Online.py
@@ -50,3 +50,30 @@ def user ():
             print("///////////////////////")
             print("TERIMA KASIH",nama,"SUDAH MEMPERCAYAI IBU KOST")
             break;
+        elif (kost == 4):
+            print("SELAMAT DATANG DI KOST KAKI\nFASILITAS: AC DAN KAMAR MANDI LUAR")
+            harga = 2000000
+            print("HARGA: 2 jt / bulan")
+            bulan = int(input("DURASI SEWA(BULAN):"))
+            ht = harga * bulan
+            print("TOTAL YANG HARUS DIBAYAR: ",ht)
+            print("///////////////////////")
+            print("TERIMA KASIH",nama,"SUDAH MEMPERCAYAI IBU KOST")
+            break;
+        else:
+            print("ERROR")
+            break;
+
+#function dengan return
+def masuk(x):
+    percobaan = 1
+    for i in range (percobaan):
+        if x == "RENAN":
+            print(f"SELAMAT DATANG {x}")
+            user()
+            break;
+        else:
+            print("ID BELUM TERDAFTAR")
+            
+        
+masuk(input("MASUKAN NAMA: "))
